71e56394278ef7a4188bba91e2659bf2bb3598cb
Yuliia Skaskevych <skaskevich2000@gmail.com>
2023-04-12 15:41:16 +0200

    hw_25_changed

--- script.js
+++ script.js
@@ -1,32 +1,37 @@
-const emojis = ["😁", "😂", "😄", "😘", "😊"];
+window.onload = function() {
 
-let votes = new Array(emojis.length).fill(0);
+    const divQuestion = document.getElementById('question');
+    const myQuestion = document.createElement('p');
+    myQuestion.textContent = 'Please choice your favourite emoji';
+    myQuestion.setAttribute('style', 'padding-left: 13px');
+    divQuestion.appendChild(myQuestion);
 
-const buttonsContainer = document.createElement("divBtns");
-const votesContainer = document.createElement("divVotes");
+    let arrayEmoticons = ["😁", "😂", "😄", "😘", "😊"];
 
-for (let i = 0; i < emojis.length; i++) {
-  const button = document.createElement("button");
+    const divParent = document.getElementById('parent');
+    divParent.setAttribute('style', 'display: flex');
 
-  button.classList.add("emoji-button");
+    for (let i = 0; i < arrayEmoticons.length; i++) {
+        const divItem = document.createElement('div');
+        divParent.appendChild(divItem);
+        divItem.setAttribute('style', 'text-align: center');
+        const emoticon = document.createElement('button');
+        divItem.appendChild(emoticon);
+        emoticon.setAttribute('style', 'cursor: pointer; border: 0; background-color: transparent; font-size: 25px');
+        emoticon.innerHTML = arrayEmoticons[i];
+        const myCount = document.createElement('p');
+        divItem.appendChild(myCount);
+        myCount.textContent = 0;
+    }
 
-  button.innerText = emojis[i];
+    divParent.addEventListener('click', (event) => {
+        let targetElement = event.target;
+        if (targetElement.tagName !== 'BUTTON') {
+            return;
+        } else {
+            let count = targetElement.nextSibling;
+            count.textContent = parseInt(count.textContent, 10) + 1;
+        }
+    });
 
-  buttonsContainer.appendChild(button);
-
-  button.addEventListener("click", function () {
-    votes[i]++;
-    updVotesDisplay();
-  });
-}
-
-document.body.appendChild(buttonsContainer);
-document.body.appendChild(votesContainer);
-
-function updVotesDisplay() {
-  let displayString = "";
-  for (let i = 0; i < emojis.length; i++) {
-    displayString += `${emojis[i]}: ${votes[i]} `;
-  }
-  votesContainer.innerText = displayString;
-}
+};
